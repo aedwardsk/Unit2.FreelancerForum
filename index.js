51ced99e371041caea25a4bc4b0677800a23b94e
Andy Edwards <andyedwards@outlook.com>
2025-01-09 20:52:28 -0500

    got some starter code

---
+++ index.js
@@ -0,0 +1,23 @@
+const freelancers = [
+  {
+    name: "Alice",
+    occupation: "Writer",
+    price: 30,
+  },
+  {
+    name: "Bob",
+    occupation: "Teacher",
+    price: 50,
+  },
+];
+
+const possibleFreelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
